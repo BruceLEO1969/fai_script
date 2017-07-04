310edff3f9c48318c95139048ea27e6570c928b9
BruceLEO1969 <BruceLEO1969@gmail.com>
2017-07-04 02:59:25 -0700

    updateed.

--- address.sh
+++ address.sh
@@ -2,8 +2,7 @@
 
 #curl -s freeapi.ipip.net/$(dig +noall +answer ${1} | awk '{print $NF}') | jq '.[] | tostring | select(length > 0)' --raw-output | sed ':a;N;$!ba;s/\n/, /g'
 
-ip=$(dig +noall +answer ${1} | awk '{print $NF}')
-location=$(curl -s freeapi.ipip.net/${ip} | jq '.[] | tostring | select(length > 0)' --raw-output | sed ':a;N;$!ba;s/\n/, /g')
+ip=$(dig +noall +answer ${1} | awk '$4=="A" {print $NF}')
+location=$(curl -s freeapi.ipip.net/${ip} | jq '.[] | tostring | select(length > 0)' --raw-output | sed ':a;N;$!ba;s/\n/  /g')
 
-echo ${ip}:
-echo ${location}
+echo ${ip} "["${location}"]"
